dab990184d27f84d94ed55118a282175c6fadd15
Rupam Sau <rupamsau0010@users.noreply.github.com>
2020-10-25 10:24:51 +0530

    General Redux Implimentation on index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+
+// ACTIONS...
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  }
+}
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+// REDUCERS
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;  
+  }
+}
+
+// CREATE STORE..
+let store = createStore(counter)
+
+// DISPLAY THE STORE IN THE CONSOLE...
+store.subscribe(() => console.log(store.getState()))
+
+// DIPATCH
+
+store.dispatch(increment())
+store.dispatch(decrement())
+store.dispatch(decrement())
 
 ReactDOM.render(
   <React.StrictMode>
